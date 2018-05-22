638b04b463b154922a6eb3f8a83189ef8194967e
sinash <pine0113@gmail.com>
2018-05-22 22:22:44 +0800

    add photo upload function restaurant

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -21,6 +21,7 @@ class Admin::RestaurantsController < ApplicationController
   end
 
 
+
   def new
     @restaurant = Restaurant.new
   end
@@ -49,7 +50,7 @@ class Admin::RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
   end
 
   def set_restaurant
