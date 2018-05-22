2f3d4105125c3ad4c712f3865aa75eaf46ea2d31
sinash <pine0113@gmail.com>
2018-05-22 20:37:39 +0800

    add restaurant view a record and admin view

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -9,6 +9,12 @@ class Admin::RestaurantsController < ApplicationController
 
   end
 
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
   def new
     @restaurant = Restaurant.new
   end
@@ -29,5 +35,5 @@ class Admin::RestaurantsController < ApplicationController
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
   end
-  
+
 end
