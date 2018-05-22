278494023ee0de451a647b4ec2d16b2f5b8e9345
sinash <pine0113@gmail.com>
2018-05-22 21:50:53 +0800

    add restaurant edit & delete action

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,17 +1,23 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_restaurant, only: [:show, :edit, :update]
 
   def index
      @restaurants = Restaurant.all
-
-
-
   end
 
 
   def show
-    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
   end
 
 
@@ -30,10 +36,24 @@ class Admin::RestaurantsController < ApplicationController
     end
   end
 
+   def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "restaurant was successfully updated"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash.now[:alert] = "restaurant was failed to update"
+      render :edit
+    end
+  end
+  
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
+
+def index
+     @restaurants = Restaurant.all
+  end
 end
