ba2d62217bd794e4e88efe7122f198af6b191e66
sinash <pine0113@gmail.com>
2018-05-23 14:36:46 +0800

    user can add comment on restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,24 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
 
-def index
-     @restaurants = Restaurant.all
+  def index
+     @restaurants = Restaurant.page(params[:page]).per(10) 
   end
+
+  def show
+    @comment = Comment.new
+    
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
